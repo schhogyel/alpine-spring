14e8608f344fa56c0b5244c46e3d4ab84bb52b1a
Sonam Chhogyel <sonam.chhogyel@empired.com>
2018-10-03 16:37:01 +1000

    update styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Notes & Scripts',
+    title: 'Vulture Speak',
   },
   plugins: [
     {
--- src/components/header.js
+++ src/components/header.js
@@ -7,16 +7,18 @@ const Header = props => (
     <div className="w-full max-w-screen-xl relative mx-auto px-6">
       <div className="flex items-center -mx-6">
         <div className="lg:w-1/4 xl:w-1/5 pl-6 pr-6 lg:pr-8" />
-        <nav class="flex flex-grow items-center lg:w-3/4 xl:w-4/5">
-          <div class="w-full lg:px-6 lg:w-3/4">
-            <span class="font-semibold text-xl tracking-tight">
-              Notes and Scripts
-            </span>
+        <nav className="flex flex-grow items-center lg:w-3/4 xl:w-4/5">
+          <div className="w-full lg:px-4 lg:w-3/4">
+            <Link to="/" className="no-underline">
+              <span className="font-semibold text-xl tracking-loose">
+                {props.siteTitle}
+              </span>
+            </Link>
           </div>
-          <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+          <div className="block lg:hidden">
+            <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
               <svg
-                class="fill-current h-3 w-3"
+                className="fill-current h-3 w-3"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -25,11 +27,11 @@ const Header = props => (
               </svg>
             </button>
           </div>
-          <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div class="text-sm lg:flex-grow">
+          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="text-sm lg:flex-grow">
               <Link
                 to="/about"
-                class="block mt-4 lg:inline-block lg:mt-0 text-blue hover:text-blue-lighter mr-4"
+                className="block mt-4 lg:inline-block lg:mt-0 text-blue hover:text-blue-lighter mr-4"
               >
                 About
               </Link>
